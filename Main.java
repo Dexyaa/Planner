ac166d1a2eadbf9c68cfe1376fcfb8e5ee6c579c
Geodude <geolundgustuf@gmail.com>
2021-11-19 16:32:38 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
+    public static String directory = System.getProperty("user.home");
+    public static String fileName = "homework.txt";
+    public static String absolutePath = directory + File.separator + fileName;
     public static void main(String[] args){
-        
+
     }
 }
