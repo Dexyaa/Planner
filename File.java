39bdae170f28902a96b18dabea6af1795830f8af
Geodude <geolundgustuf@gmail.com>
2021-11-19 20:53:09 -0600

    Update File.java

--- File.java
+++ File.java
@@ -1,12 +1,32 @@
 import java.io.*;
-public class File {
+public class File{
 
-
-    public void reader(String path){
-
+    File(){
+        
     }
 
-    public void writer(String path){
+    public void reader(String path){
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(path))) {
+            String line = bufferedReader.readLine();
+            while(line != null) {
+                System.out.println(line);
+                line = bufferedReader.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            // Exception handling
+            System.out.println("File: " + path + " was not found.");
+        } catch (IOException e) {
+            // Exception handling
+            e.printStackTrace();
+        }
+    }
 
+    public void writer(String path, String input){
+        try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(path))) {
+            bufferedWriter.write(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Exception handling
+        }
     }
 }
